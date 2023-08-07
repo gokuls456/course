8c1e8fca6b40c10fe94ecdcbfc4dec10e4449131
gokuls456 <gokulduki23@gmail.com>
2023-08-07 11:25:22 -0700

    pranesh da

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js or any other main file
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import './App.css';
 import Program from './main/program/index';
